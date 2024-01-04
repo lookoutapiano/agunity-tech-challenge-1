286f5365d2deb543b33883bf3d4657293ecfdc95
Abdulmaula Nacan <abdulmaula.nacan@gmail.com>
2024-01-04 08:35:05 +0800

    first commit

---
+++ app.ts
@@ -0,0 +1,35 @@
+async function getWeatherByCoordinates() {
+    const apiKey = "88c9bc9fb353f01f3aeb6221add0a59e";
+    const latitudeInput = document.getElementById("latitudeInput") as HTMLInputElement;
+    const longitudeInput = document.getElementById("longitudeInput") as HTMLInputElement;
+    const weatherInfoElement = document.getElementById("weatherInfo");
+
+    const latitude = latitudeInput.value.trim(); //removes white space
+    const longitude = longitudeInput.value.trim(); //removes white space
+
+    if (latitude === "" || longitude === "") {
+        alert("Please enter both latitude and longitude");
+        return;
+    }
+
+    if (weatherInfoElement) {
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+            );
+            const data = await response.json();
+
+            const { name, main, weather } = data;
+            const temperature = main.temp;
+            const description = weather[0].description;
+            weatherInfoElement.classList.remove("danger");
+            weatherInfoElement.innerHTML = `<p>Location: ${name}</p><p>Temperature: ${temperature}°C</p><p>Description: ${description}</p>`;
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+            weatherInfoElement.classList.add("danger");
+            weatherInfoElement.innerHTML = "Failed to fetch weather data. Please try again.";
+        }
+    } else {
+        console.error('Element with ID "weatherInfo" not found in the DOM');
+    }
+}
